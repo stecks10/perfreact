6afa4e88a0891b9818c78cb452298c5ccb43940c
Vitor Nunes <stecks10@gmail.com>
2021-10-29 10:28:51 -0300

    Memo

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -17,3 +17,16 @@ export function SearchResults({ results }: SearchResultsProps) {
     </div>
   );
 }
+
+/**
+ * 1. Criar uma nova versão do component
+ * 2. Comparar com a versão anterior
+ * 3. Se houverem alterações, vai atualizar o que alterou
+ *
+ *
+ * uso do memo
+ * 1. Pure functional components
+ * 2. Renders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size
+ */
