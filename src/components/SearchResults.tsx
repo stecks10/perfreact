2944abc0f058207d703e65faa4694be6d206fe15
Vitor Nunes <stecks10@gmail.com>
2021-10-29 17:46:02 -0300

    Virtualização

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { List, ListRowRenderer } from 'react-virtualized';
 
 import { ProductItem } from './ProductItem';
 
@@ -21,19 +21,29 @@ export function SearchResults({
   totalPrice,
   onAddToWishList,
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map((product) => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWishList={onAddToWishList}
-          />
-        );
-      })}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanColumnCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
