2e2c44241157d4ff8a18509f5b2ddf4551325e51
Vitor Nunes <stecks10@gmail.com>
2021-10-29 17:52:59 -0300

    Bundle Analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({});
--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,6 +1,7 @@
 import { memo, useState } from 'react';
 import { AddProductToWishlistProps } from './AddProductToWishlist';
 import dynamic from 'next/dynamic';
+import lodash from 'lodash';
 
 const AddProductToWishlist = dynamic<AddProductToWishlistProps>(
   () => {
@@ -48,6 +49,6 @@ function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
 export const ProductItem = memo(
   ProductItemComponent,
   (prevProps, nextProps) => {
-    return Object.is(prevProps.product, nextProps.product);
+    return lodash.isEqual(prevProps.product, nextProps.product);
   }
 );
